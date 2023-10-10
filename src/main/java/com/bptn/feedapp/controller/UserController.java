f7b2e1f92902e0184eb70873af2b724402c93d97
sweythak <sweythak@gmail.com>
2023-10-10 11:56:52 -0400

    Created authenticate method in UserService class

--- src/main/java/com/bptn/feedapp/controller/UserController.java
+++ src/main/java/com/bptn/feedapp/controller/UserController.java
@@ -21,42 +21,41 @@ import com.bptn.feedapp.service.UserService;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
-	
+
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@GetMapping("/test")
 	public String testController() {
-		
-		logger.debug("The testController() method was invoked!");	
+
+		logger.debug("The testController() method was invoked!");
 		return "The feed Application is up and running";
-		
+
 	}
-	
+
 	@GetMapping("/")
 	public List<User> listUsers() {
-		
+
 		logger.debug("The listUsers() method was invoked!");
 		return this.userService.listUsers();
 	}
-	
+
 	@GetMapping("/{username}")
 	public Optional<User> findByUsername(@PathVariable String username) {
-		
+
 		logger.debug("The findByUsername() method was invoked!, username={}", username);
 		return this.userService.findByUsername(username);
-		
+
 	}
 
-	
 	@GetMapping("/{first}/{last}/{username}/{password}/{phone}/{emailId}")
-	public String createUser(@PathVariable String first, @PathVariable String last, @PathVariable String username, @PathVariable String password, @PathVariable String phone, @PathVariable String emailId) {
-		
+	public String createUser(@PathVariable String first, @PathVariable String last, @PathVariable String username,
+			@PathVariable String password, @PathVariable String phone, @PathVariable String emailId) {
+
 		User user = new User();
-		
+
 		user.setFirstName(first);
 		user.setLastName(last);
 		user.setUsername(username);
@@ -65,28 +64,30 @@ public class UserController {
 		user.setEmailId(emailId);
 		user.setEmailVerified(false);
 		user.setCreatedOn(Timestamp.from(Instant.now()));
-				
+
 		logger.debug("The createUser() method was invoked!, user={}", user.toString());
-				
+
 		this.userService.createUser(user);
-				
+
 		return "User Created Successfully";
-		
-     }
-	
-	
-	
-	
-	@PostMapping("/signup")
-	public User signup(@RequestBody User user) {	
-		
-		logger.debug("Signing up, username: {}", user.getUsername());
-		
-		return this.userService.signup(user);
-		
+
 	}
-	
-	
+
+	@PostMapping("/signup")
+	public User signup(@RequestBody User user) {
+
+		logger.debug("Signing up, username: {}", user.getUsername());
+
+		return this.userService.signup(user);
+
+	}
+
+	@GetMapping("/verify/email")
+	public void verifyEmail() {
+			
+		logger.debug("Verifying Email");
+			
+		this.userService.verifyEmail();
+	}
 
 }
-
--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -6,13 +6,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.bptn.feedapp.exception.domain.EmailExistException;
+import com.bptn.feedapp.exception.domain.EmailNotVerifiedException;
+import com.bptn.feedapp.exception.domain.UserNotFoundException;
 import com.bptn.feedapp.exception.domain.UsernameExistException;
 import com.bptn.feedapp.jpa.User;
+import com.bptn.feedapp.provider.ResourceProvider;
 import com.bptn.feedapp.repository.UserRepository;
+import com.bptn.feedapp.security.JwtService;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import org.springframework.http.HttpHeaders;
 
 @Service
 public class UserService {
@@ -22,10 +32,18 @@ public class UserService {
 
 	@Autowired
 	EmailService emailService;
-	
+
 	@Autowired
 	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	AuthenticationManager authenticationManager;
 	
+	@Autowired
+	JwtService jwtService;
+
+	@Autowired
+	ResourceProvider provider;
 
 	public List<User> listUsers() {
 
@@ -57,19 +75,19 @@ public class UserService {
 		user.setUsername(user.getUsername().toLowerCase());
 		user.setEmailId(user.getEmailId().toLowerCase());
 
-		//check if username or emailId is existing and handle it with exceptions
+		// check if username or emailId is existing and handle it with exceptions
 		this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
 
-		//encrypt the password to save it to the DB
+		// encrypt the password to save it to the DB
 		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		user.setEmailVerified(false);
 		user.setCreatedOn(Timestamp.from(Instant.now()));
 
-		//creating the user in the DB using INSERT operation
-	    this.userRepository.save(user);
+		// creating the user in the DB using INSERT operation
+		this.userRepository.save(user);
 
 		this.emailService.sendVerificationEmail(user);
-		
+
 		return user;
 	}
 
@@ -83,6 +101,47 @@ public class UserService {
 			throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
 		});
 
+	}
+
+	public void verifyEmail() {
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		User user = this.userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+
+		user.setEmailVerified(true);
+
+		this.userRepository.save(user);
+	}
+
+	private static User isEmailVerified(User user) {
+
+		if (user.getEmailVerified().equals(false)) {
+			throw new EmailNotVerifiedException(String.format("Email requires verification, %s", user.getEmailId()));
+		}
+
+		return user;
+	}
+
+	private Authentication authenticate(String username, String password) {
+		return this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+	}
+	
+	public User authenticate(User user) {
+
+		/* Spring Security Authentication. */
+		this.authenticate(user.getUsername(), user.getPassword());
+
+		/* Get User from the DB. */
+		return this.userRepository.findByUsername(user.getUsername()).map(UserService::isEmailVerified).get();
+	}
+	
+	public HttpHeaders generateJwtHeader(String username) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username,this.provider.getJwtExpiration()));
+
+		return headers;
 	}
 
 }
