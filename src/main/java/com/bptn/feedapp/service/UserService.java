fa83a2bc8e7a110b4dab8f9c36533450c040194b
sweythak <sweythak@gmail.com>
2023-10-10 09:38:24 -0400

    Updated UserService class

--- src/main/java/com/bptn/feedapp/service/UserService.java
+++ src/main/java/com/bptn/feedapp/service/UserService.java
@@ -6,8 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.bptn.feedapp.exception.domain.EmailExistException;
+import com.bptn.feedapp.exception.domain.UsernameExistException;
 import com.bptn.feedapp.jpa.User;
 import com.bptn.feedapp.repository.UserRepository;
 
@@ -16,24 +19,26 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	    
+
 	@Autowired
 	EmailService emailService;
-	    
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 
 	public List<User> listUsers() {
 
-		//this calls findAll() method from CrudRepository(superInterface for
+		// this calls findAll() method from CrudRepository(superInterface for
 		// JpaRepository)
-		//this is internally firing "Select * from "\"User\"";
+		// this is internally firing "Select * from "\"User\"";
 		return this.userRepository.findAll();
 	}
 
 	public Optional<User> findByUsername(String username) {
 
 		// this calls findByUsername() method from UserRepository interface
-		//this is internally firing "Select * from "\"User\"" where "username"=?;
+		// this is internally firing "Select * from "\"User\"" where "username"=?;
 		return this.userRepository.findByUsername(username);
 
 	}
@@ -46,21 +51,38 @@ public class UserService {
 		// or an update operation for an already inserted record
 		this.userRepository.save(user);
 	}
-	
-	public User signup(User user){
-		
+
+	public User signup(User user) {
+
 		user.setUsername(user.getUsername().toLowerCase());
 		user.setEmailId(user.getEmailId().toLowerCase());
+
+		//check if username or emailId is existing and handle it with exceptions
+		this.validateUsernameAndEmail(user.getUsername(), user.getEmailId());
+
+		//encrypt the password to save it to the DB
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		user.setEmailVerified(false);
 		user.setCreatedOn(Timestamp.from(Instant.now()));
-		
-		//performing an insert operation
-		this.userRepository.save(user);
-		
-		this.emailService.sendVerificationEmail(user);	    
+
+		//creating the user in the DB using INSERT operation
+	    this.userRepository.save(user);
+
+		this.emailService.sendVerificationEmail(user);
 		
 		return user;
-		
+	}
+
+	private void validateUsernameAndEmail(String username, String emailId) {
+
+		this.userRepository.findByUsername(username).ifPresent(u -> {
+			throw new UsernameExistException(String.format("Username already exists, %s", u.getUsername()));
+		});
+
+		this.userRepository.findByEmailId(emailId).ifPresent(u -> {
+			throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));
+		});
+
 	}
 
 }
