2f49d03ddbe54cf48a4dd1fb93cb36fca1cf508f
sweythak <sweythak@gmail.com>
2023-10-13 14:16:23 -0400

    Modified spring boot test class

--- src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
+++ src/test/java/com/bptn/feedapp/FeedAppApplicationTests.java
@@ -1,13 +1,23 @@
 package com.bptn.feedapp;
 
+import org.h2.tools.Server;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FeedAppApplicationTests {
 
+	@DisplayName("Demo Test")
 	@Test
 	void contextLoads() {
+		
 	}
+	
+	@MockBean
+	Server server;
 
 }
